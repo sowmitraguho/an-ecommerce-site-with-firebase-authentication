fdc40ef8df5b52f45ee2da0a32604bb4c848bd65
Sowmitra Guho <sowmitraguho@gmail.com>
2023-09-28 22:25:35 +0600

    tailwind and daisyui installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './App.css'
+import Main from "./LayOut/Main";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Orders from "./Components/Orders/Orders";
+import Login from "./Components/LogIn/Login";
+import SignUp from "./Components/SignUp/SignUp";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>
+        },
+        {
+          path: "/about",
+          element: <About></About>
+        },
+        {
+          path: "/orders",
+          element: <Orders></Orders>
+        },
+        {
+          path: "/login",
+          element: <Login></Login>
+        },
+        {
+          path: "/signup",
+          element: <SignUp></SignUp>
+        },
+      ]
+    },
+  ]);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+     <RouterProvider router={router}></RouterProvider>
+    </div>
   )
 }
 
