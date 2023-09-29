6c6e1a21626d7250c3677b5e876c913bdfbda326
Sowmitra Guho <sowmitraguho@gmail.com>
2023-09-29 23:43:17 +0600

    Bata Shop Cloning Started and home page half done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,36 +3,162 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <section className="">
-      
-      <section className="navbar bg-primary text-primary-content rounded-lg  justify-between">
-        <div>
-        <Link to="/" className="btn btn-ghost normal-case text-xl">
-          <img src="../../../image/logo.png" alt="" srcset="" />
-        </Link>
+    <section className="mx-6 p-6">
+      <header className="flex justify-between">
+        <div className="inline">
+          <Link to={""} className="w-1/12 inline">
+            <svg
+              className="w-[2rem] inline"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 256 256"
+            >
+              <rect width="2" height="2" fill="none" />
+              <circle
+                cx="128"
+                cy="104"
+                r="32"
+                fill="none"
+                stroke="#000"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="8"
+              />
+              <path
+                d="M208,104c0,72-80,128-80,128S48,176,48,104a80,80,0,0,1,160,0Z"
+                fill="none"
+                stroke="#000"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="8"
+              />
+            </svg>
+            Find a store | Customer Care: 09666200300
+          </Link>
         </div>
-        <div>
-        <Link to="/" className="btn btn-ghost normal-case text-xl">
-          Home
-        </Link>
-        <Link to="/about" className="btn btn-ghost normal-case text-xl">
-          About
-        </Link>
-        <Link to="/orders" className="btn btn-ghost normal-case text-xl">
-          Order
-        </Link>
-        <Link to="/shop" className="btn btn-ghost normal-case text-xl">
-          Shop
-        </Link>
-        <Link to="/login" className="btn btn-ghost normal-case text-xl">
-          LogIn
-        </Link>
-        <Link to="/signup" className="btn btn-ghost normal-case text-xl">
-          SignUp
-        </Link>
+
+        <div className="inline">
+          <Link to="/" className="inline">
+            <img
+              className="inline"
+              src="../../../image/logo.png"
+              alt=""
+              srcset=""
+            />
+          </Link>
+        </div>
+
+        <div className="inline-block w-1/3 flex justify-between">
+          <Link className="inline">
+            <svg
+              className="inline"
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+            </svg>
+          </Link>
+          <Link className="inline">
+            <svg
+              className="inline"
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 512 512"
+            >
+              <style>svg{}</style>
+              <path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" />
+            </svg>
+          </Link>
+          <Link>
+            <svg
+              className="inline fill-lime-500 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <style></style>
+              <path d="M224 122.8c-72.7 0-131.8 59.1-131.9 131.8 0 24.9 7 49.2 20.2 70.1l3.1 5-13.3 48.6 49.9-13.1 4.8 2.9c20.2 12 43.4 18.4 67.1 18.4h.1c72.6 0 133.3-59.1 133.3-131.8 0-35.2-15.2-68.3-40.1-93.2-25-25-58-38.7-93.2-38.7zm77.5 188.4c-3.3 9.3-19.1 17.7-26.7 18.8-12.6 1.9-22.4.9-47.5-9.9-39.7-17.2-65.7-57.2-67.7-59.8-2-2.6-16.2-21.5-16.2-41s10.2-29.1 13.9-33.1c3.6-4 7.9-5 10.6-5 2.6 0 5.3 0 7.6.1 2.4.1 5.7-.9 8.9 6.8 3.3 7.9 11.2 27.4 12.2 29.4s1.7 4.3.3 6.9c-7.6 15.2-15.7 14.6-11.6 21.6 15.3 26.3 30.6 35.4 53.9 47.1 4 2 6.3 1.7 8.6-1 2.3-2.6 9.9-11.6 12.5-15.5 2.6-4 5.3-3.3 8.9-2 3.6 1.3 23.1 10.9 27.1 12.9s6.6 3 7.6 4.6c.9 1.9.9 9.9-2.4 19.1zM400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM223.9 413.2c-26.6 0-52.7-6.7-75.8-19.3L64 416l22.5-82.2c-13.9-24-21.2-51.3-21.2-79.3C65.4 167.1 136.5 96 223.9 96c42.4 0 82.2 16.5 112.2 46.5 29.9 30 47.9 69.8 47.9 112.2 0 87.4-72.7 158.5-160.1 158.5z" />
+            </svg>
+          </Link>
+          <Link>
+            <label tabIndex={0} className="btn btn-ghost btn-circle inline">
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-sm indicator-item">8</span>
+              </div>
+            </label>
+          </Link>
+          <div className="static">
+            <input
+              type="text"
+              id="search-navbar"
+              className="inline p-2 pl-10 text-sm text-gray-900 border border-gray-300  bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+              placeholder="Search..."
+            />
+            <div className="absolute top-12 pl-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </header>
+      <section className="navbar">
+        <div className="flex justify-center w-full">
+          <Link to="/" className="text-red-500 p-6 uppercase ">
+            Sneaker Studio
+          </Link>
+          <Link to="/about" className="p-6 uppercase ">
+            Brands
+          </Link>
+          <Link to="/orders" className="p-6 uppercase ">
+            Men
+          </Link>
+          <Link to="/orders" className="p-6 uppercase ">
+            Women
+          </Link>
+          <Link to="/orders" className="p-6 uppercase ">
+            Children
+          </Link>
+          <Link to="/orders" className="p-6 uppercase ">
+            Accessories
+          </Link>
+          <Link to="/shop" className="p-6 uppercase ">
+            BATA Club
+          </Link>
+          <Link to="/login" className="p-6 uppercase ">
+            B-Mag
+          </Link>
+          <Link to="/signup" className="p-6 uppercase ">
+            Franchise
+          </Link>
         </div>
       </section>
-      
     </section>
   );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import { Carousel } from "react-daisyui";
+import CarouselBanner from "../CarouselBanner/CarouselBanner";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>This is home</h2>
+  return (
+    <div>
+      <CarouselBanner></CarouselBanner>
+      <div id="classification" className="flex justify-between my-6">
+        <div className="card card-compact w-96 bg-base-100 shadow-xl mx-3 rounded-none">
+          <figure>
+            <img className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 hover:grayscale"
+              src="../../../image/card1.jpg"
+              alt="Shoes"
+            />
+          </figure>
         </div>
-    );
+        <div className="card card-compact w-96 bg-base-100 shadow-xl mx-3 rounded-none">
+          <figure>
+            <img className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..."
+              src="../../../image/card2.jpg"
+              alt="Shoes"
+            />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl mx-3 rounded-none">
+          <figure>
+            <img className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..."
+              src="../../../image/card3.jpg"
+              alt="Shoes"
+            />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl mx-3 rounded-none">
+          <figure>
+            <img className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..."
+              src="../../../image/card4.jpg"
+              alt="Shoes"
+            />
+          </figure>
+        </div>
+      </div>
+      <div id="voucher" className="mx-3 my-6">
+        <img src="../../../image/voucher.jpg" alt="" srcset="" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
